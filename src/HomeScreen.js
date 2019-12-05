dc2b97e938f3e24dfd86488a846c5326b7468cf4
irshadvali <irshad.vali@gmail.com>
2019-12-05 22:48:51 +0530

    es lint

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,31 +1,45 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable react-native/no-inline-styles */
 //import liraries
-import React, { Component,useState } from 'react';
-import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
+import React, {Component, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 // create a component
 function HomeScreen() {
-console.log("====================")
-    const [data,setData]=useState("irshad")
-changeData=()=>{
-    setData("vali")
-}
-   return (
-     <View style={styles.container}>
-       <Text>{name}</Text>
-       <TouchableOpacity onPress={changeData}><View style={{height:50,width:200,backgroundColor:"#d00000",marginTop:40,alignItems:"center",justifyContent:"center"}}><Text>click</Text></View></TouchableOpacity>
-     </View>
-   );
-
+  const [data, setData] = useState({name: 'irshad'});
+  changeData = () => {
+    console.log('====================');
+    setData({name: 'vali'});
+  };
+  return (
+    <View style={styles.container}>
+      <Text>{data.name}</Text>
+      <TouchableOpacity onPress={changeData}>
+        <View
+          style={{
+            height: 50,
+            width: 200,
+            backgroundColor: '#d00000',
+            marginTop: 40,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text>click</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 // define your styles
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   backgroundColor: '#2c3e50',
- },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
 });
 
 //make this component available to the app
